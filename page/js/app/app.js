7d2857758439c310c63d3e72784d9b1c073bfc1e
Leonardo Fuenmayor <leof2303@gmail.com>
2016-04-30 12:11:31 -0430

    commit estructura SPA

--- page/js/app/app.js
+++ page/js/app/app.js
@@ -1,1 +1,7 @@
-angular.module('boxit', ['ngToast','ui.bootstrap']);
+angular.module('boxit', ['ngToast','ui.bootstrap','ui.router'])
+        .config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider) {
+    $urlRouterProvider.otherwise('/userInterface');
+    $stateProvider.state('userMenu', {
+        url: '/userMenu',
+        templateUrl: 'viewa/userMenu.html'
+    })}]);
--- page/js/controllers/siginController.js
+++ page/js/controllers/siginController.js
@@ -3,17 +3,21 @@
  */
 angular
     .module('boxit')
-    .controller('siginController', ['$scope', '$http','$window','ngToast','userData',
-        function ($scope, $http,$window,ngToast,userData) {
-            $scope.today = function() {
-                $scope.dt = new Date();
+    .controller('siginController', ['$scope', '$http', '$window', 'ngToast', 'userData',
+        function ($scope, $http, $window, ngToast, userData) {
+            $scope.today = function () {
+                $scope.popup1 = {
+                    opened: false
+                };
+                $scope.UserBirthdate = new Date();
             };
             $scope.today();
+            $scope.format = 'yyyy-MM-dd';
             $scope.dateOptions = {
                 dateDisabled: disabled,
-                formatYear: 'yy',
-                maxDate: new Date(2020, 5, 22),
-                minDate: new Date(),
+                formatYear: 'yyyy',
+                maxDate: new Date(2020, 1, 1),
+                minDate: new Date(1915,1,1),
                 startingDay: 1
             };
             $scope.plataformas = [];
@@ -28,25 +32,22 @@ angular
             }, function error(results) {
                 console.log(results.data);
             });
-            $scope.open = function() {
+            $scope.open = function () {
                 $scope.popup1.opened = true;
-            };
-            $scope.popup1 = {
-                opened: false
             };
             function disabled(data) {
                 var date = data.date,
                     mode = data.mode;
                 return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
             }
+
             $scope.Sigin = function () {
-              
                 var args = {};
                 args["UserName"] = $scope.username;
                 args["UserLastName"] = $scope.lastname;
                 args["UserEmail"] = $scope.useremail;
                 args["UserPassword"] = $scope.password;
-                args["IdPlataforma"] = $scope.descPlataforma != null ? 
+                args["IdPlataforma"] = $scope.descPlataforma != null ?
                     $scope.descPlataforma.attributes.IdPlataforma : null;
                 $http({
                     method: "POST",
@@ -60,35 +61,21 @@ angular
                         ngToast.create(JSON.stringify(result.data.attributes.Message));
                     }
                     else {
-                     // ngToast.create(JSON.stringify(result.data.attributes.Message));
-<<<<<<< b59590672c49d439ac22c6b95666e02edd376cf6
-                       //alert(JSON.stringify(result.data.attributes.Message)); 
-                        var user = $scope;
-=======
-                       alert(JSON.stringify(result.data.attributes.Message)); 
                         var user = {};
                         user["IdCliente"] = result.data.attributes.IdCliente;
                         user["UserName"] = $scope.username;
                         user["UserLastName"] = $scope.lastname;
                         user["UserGender"] = $scope.UserGender;
-                        user["UserBirthdate"] = $scope.UserBirthdate;
+                        user["UserBirthdate"] = moment($scope.UserBirthdate).format('DD-MMMM-YYYY');
                         user["IdPlataforma"] = $scope.IdPlataforma;
                         user["UserEmail"] = $scope.useremail;
                         user["UserPhone"] = $scope.useremail;
-
->>>>>>> ada0eb97748e0cb093d3050319e4b080afb34811
                         userData.activateUser(result.data.attributes.IdCliente);
                         userData.updateData(user);
                         $window.location = "/Iniciarsesion.html";
                     }
-
                 }, function error(result) {
                     console.log(result.data);
                 });
             }
-            
-            
-            
-            
-            
         }]);
