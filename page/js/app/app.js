a47c52033786f006865bb2d7d9d32b24a7833d0d
Leonardo Fuenmayor <leof2303@gmail.com>
2016-05-01 18:46:16 -0400

    commit app

--- page/js/app/app.js
+++ page/js/app/app.js
@@ -4,28 +4,31 @@ angular.module('boxit', ['ngToast', 'ui.bootstrap', 'ui.router'])
         $stateProvider.state('userMenu', {
             url: '/userInterface',
             templateUrl: 'views/userMenu.html',
-            controller:'userMenuController'
-        }).state('userMenu.inicio',{
-            url:'/inicio',
-            templateUrl:'views/inicio.html',
-            controller:'inicioController'
-        }).state('userMenu.editarPerfil',{
-            url:'/editarPerfil',
-            templateUrl:'views/editarPerfil.html'
-        }).state('userMenu.modificarContrasena',{
-            url:'/modificarContrasena',
-            templateUrl:'views/modificarContrasena.html'
-        }).state('userMenu.seleccionarBoxIt',{
-            url:'/seleccionarBoxIt',
-            templateUrl:'views/seleccionarBoxIt.html'
-        }).state('userMenu.anunciarPaquete',{
-            url:'/anunciarPaquete',
-            templateUrl:'views/anunciarPaquete.html'
-        }).state('userMenu.trakingDePaquetes',{
-            url:'/trakingDePaquetes',
-            templateUrl:'views/trakingDePaquetes.html'
-        }).state('userMenu.cerrarSesion',{
-            url:'/cerrarSesion',
-            templateUrl:'views/cerrarSesion.html'
+            controller: 'userMenuController'
+        }).state('userMenu.inicio', {
+            url: '/inicio',
+            templateUrl: 'views/inicio.html',
+            controller: 'inicioController'
+        }).state('userMenu.editarPerfil', {
+            url: '/editarPerfil',
+            templateUrl: 'views/editarPerfil.html'
+        }).state('userMenu.modificarContrasena', {
+            url: '/modificarContrasena',
+            templateUrl: 'views/modificarContrasena.html'
+        }).state('userMenu.seleccionarBoxIt', {
+            url: '/seleccionarBoxIt',
+            templateUrl: 'views/seleccionarBoxIt.html'
+        }).state('userMenu.anunciarPaquete', {
+            url: '/anunciarPaquete',
+            templateUrl: 'views/anunciarPaquete.html'
+        }).state('userMenu.trakingDePaquetes', {
+            url: '/trakingDePaquetes',
+            templateUrl: 'views/trakingDePaquetes.html'
+        }).state('userMenu.cerrarSesion', {
+            url: '/cerrarSesion',
+            templateUrl: 'views/cerrarSesion.html'
+        }).state('shoppingCar', {
+            url: '/shoppingCar',
+            templateUrl: 'views/shoppingCar.html'
         });
     }]);
