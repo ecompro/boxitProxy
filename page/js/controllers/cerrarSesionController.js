850aaaaea4509ed08f9fee2cf8fc027b8d909cc7
ecompro <ecomprotech@gmail.com>
2016-05-04 07:23:44 -0430

    mas vistas y controllers falta el historico de tracking y el controller de tracking

--- page/js/controllers/cerrarSesionController.js
+++ page/js/controllers/cerrarSesionController.js
@@ -12,5 +12,5 @@ angular.module('boxit')
             userData.logoff();
             $interval(function () {
                 $window.location = "/Iniciarsesion.html";
-            },3000);
+            },5000);
         }]);
