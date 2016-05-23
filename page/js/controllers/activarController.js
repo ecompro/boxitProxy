d7a01e19f02e3d3fa309830090c206454d59ed35
ecompro <ecomprotech@gmail.com>
2016-05-22 20:27:10 -0400

    mas mensajes e icono del store

--- page/js/controllers/activarController.js
+++ page/js/controllers/activarController.js
@@ -9,7 +9,7 @@ angular
         .module('boxit')
         .controller('activarController', ['$scope', '$http', '$stateParams', 'md5', 'userData', '$uibModal',
             function ($scope, $http, $stateParams, md5, userData, $uibModal) {
-                $scope.exitoso = true;
+                $scope.exitoso = false;
 
                 $scope.init = function () {
 
@@ -17,18 +17,20 @@ angular
                     var IdCliente = hash[0];
                     var token = hash[1];
                     var calculatedHash = md5.createHash(IdCliente.toString() + "myBoxIT216" || '');
-                    $scope.exitoso = true;
+                    
 
                     if (token === calculatedHash){
                         userData.activateUser(IdCliente).then(function success(result){
                             var respuesta = "Hubo un error en la activación del usuario, intente mas tarde o contactenos.";
                             
-                            if(result = "Success"){
-                                respuesta ="El usuario fue activado con exito, por favor inicie sesión.";
+                            if(result === "Success"){
+                                respuesta ="Usted recibirá un correo electrónico con su P.O BOX en Miami. El cual deberá usar en todas las páginas de compra como su dirección de envío.";
                                 
                                 
                             }
+                            
                             $scope.mensaje = respuesta;
+                            $scope.exitoso = true;
                             
                         }
                         , function error(result){
